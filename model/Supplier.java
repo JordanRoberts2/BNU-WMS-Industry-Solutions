d7ee540e3a390f6e8748d46295aa702bd7d371d5
“Jordan <jtdroberts@hotmail.com>
2025-05-16 18:37:15 +0100

    Added the prperties for name, contact and ID.

---
+++ model/Supplier.java
@@ -0,0 +1,41 @@
+package model;
+
+public class Supplier {
+
+    private String name;
+    private int id;
+    private String contact;
+    // order history tbc
+
+    public Supplier(int id, String name, String contact) {
+        this.id = id;
+        this.name = name;
+        this.contact = contact;
+    } 
+//Getter and setter for ID
+    public int getId(){
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+//Getter and setter for Name
+    public String getName(){
+        return name;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+// Getter and setter for Contact
+
+    public String getContact(){
+        return contact;
+    }
+    
+    public void setContact(String contact){
+        this.contact = contact;
+    }
+}
